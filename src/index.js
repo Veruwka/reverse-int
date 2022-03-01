0716ae9951f07124c1d5c712853c13f232d83c0b
discord VeraTiutiunnik#4076 <96355243+Veruwka@users.noreply.github.com>
2022-03-01 22:27:44 +0300

    reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  if(n < 0) n = n * (-1);
+  let newArray = [];
+  let nArray = Array.from(String(n));
+  newArray = nArray.reverse();
+  return (Number(newArray.join('')));
 }
